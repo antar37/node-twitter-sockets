67f1a2f2e8443a5103ab7383edcd079207b3a08d
Alex Tarasiuk <antar37@gmail.com>
2018-10-11 19:09:18 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,41 @@
+// To begin, run npm init, and fill out the information
+// To install the needed packages, run in the terminal
+// npm install express --save
+// npm install socket.io --save
+// npm install twit -- save
+
+var express = require('express')
+  , app = express()
+  , http = require('http')
+  , server = http.createServer(app)
+  ,Twit = require('twit')
+  , io = require('socket.io').listen(server);
+
+server.listen(8080);
+
+// routing
+app.get('/', function (req, res) {
+res.sendFile(__dirname + '/index.html');
+});
+
+var watchList = ['Apple', 'Banana', 'Vanilla', 'Recipe'];
+ var T = new Twit({
+    consumer_key:         ''
+  , consumer_secret:      ''
+  , access_token:         ''
+  , access_token_secret:  ''
+})
+
+io.sockets.on('connection', function (socket) {
+  console.log('Connected');
+
+
+ var stream = T.stream('statuses/filter', { track: watchList })
+
+  stream.on('tweet', function (tweet) {
+
+    io.sockets.emit('stream',tweet.text);
+    console.log(tweet.text);
+
+  });
+ });
